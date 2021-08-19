4ccae364d153f851ae10940412195bcbc90ff1b9
Centeno Jose Antonio <jocecito182@gmail.com>
2021-08-19 09:28:05 -0300

    changes

--- includes/database.php
+++ includes/database.php
@@ -3,11 +3,11 @@
 $db = mysqli_connect('192.168.100.3', 'root', '', 'appsalon');
 
 // Mi IP 192.168.100.3
- if(!$db) {
-    echo "Error en la conexión";
- } else {
-     echo "Conexión Correcta";
- }
+//  if(!$db) {
+//     echo "Error en la conexión";
+//  } else {
+//      echo "Conexión Correcta";
+//  }
 
 if(!$db) {
     echo "Error en la conexión";
